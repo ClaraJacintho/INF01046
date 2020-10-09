9cc7d835913e5a01abcf290e0d9f6e64ef9541cd
Clara Jacintho <mcmachry@gmail.com>
2020-10-09 00:56:17 -0300

    started contrast stretching implementation

--- Q2/contrast_stretching.m
+++ Q2/contrast_stretching.m
@@ -1,3 +1,17 @@
-function [I_out, alfa, beta, a, b] =contrast_stretching(I_in)
+function I_out =contrast_stretching(I_in, a, b, alfa, beta, gamma, va, vb)
 
- 
+[M, N]=size(I_in);
+I_out=I_in;
+
+ for i=1:M
+     for j=1:N
+           if I_in(i,j) >= a
+               I_out(i,j) = I_in(i,j)*alfa;
+           elseif I_in(i,j) < b
+               I_out(i,j) = beta*(I_in(i,j) - a) + va;
+           else
+               I_out(i,j) = gamma*(I_in(i,j) - b) + vb;
+           end;
+     end;
+ end;
+ figure, imshow(uint8(I_out))
--- Q2/q2.m
+++ Q2/q2.m
@@ -1,7 +1,12 @@
 % Parte 1 - contrast_stretching com imagens preto e branco
-I_ny = imread('images/ny.png');
-I_ma = imread('images/morocco.png');
-imhist(I_ma)
+% I_ny = imread('images/ny.png');
+% I_ma = imread('images/morocco.png');
 % Parte 2 - contrast_stretching em cada canal de imagens RGB
 %I_at = imread('images/hallstatt.jpg');
 %I_dk = imread('images/copenhagen.jpg');
+
+A = imread('tire.tif');
+figure,imshow(A)
+figure,imhist(A)
+B = contrast_stretching(A, 50, 150, 0.2, 2, 1, 30, 200)
+figure,imhist(B)
